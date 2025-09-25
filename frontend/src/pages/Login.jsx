f7e8193519dbd094ef10ea8e48311ecd676521d4
Chedrick Daniel Uzaga López <80350684+Chedrick343@users.noreply.github.com>
2025-09-25 12:57:36 -0600

    Haciendolo responsive para tablet, ya quedo listo, ahora sólo falta hacerlo responsive para desktop

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,20 +3,23 @@ import logo from '../assets/Damena-logo-original.png'
 
 function Login(){
     return(
-        <section id='contenedorSeccion'>
-            <img id='logoDamena' src={logo} alt="Imagen del logo de banco Damena en su versión clara" />
-            <form action="" id="contenedorCredenciales">
-                <label htmlFor="">Usuario</label>
-                <input type="text" />
-                <label htmlFor="">Contraseña</label>
-                <input type="password" />
-                <button id='botonIniciarSesion'>Iniciar sesión</button>
-            </form>
-            <div id='divisionAyuda'>
-                <a href="">Olvidé mi contraseña</a> <p>|</p> <a href="">Ayuda</a>
-            </div>
-        </section>
-
+        <div id='contenedorPagina'>
+            <section id='contenedorSeccion'>
+                <img id='logoDamena' src={logo} alt="Imagen del logo de banco Damena en su versión clara" />
+                <h2>Log in</h2>
+                <form action="" id="contenedorCredenciales">
+                    <label htmlFor="">Usuario</label>
+                    <input type="text" placeholder='Ejm: Usuario123'/>
+                    <label htmlFor="">Contraseña</label>
+                    <input type="password" />
+                    <button id='botonIniciarSesion'>Iniciar sesión</button>
+                </form>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio quae, esse quaerat dignissimos magni vero expedita veritatis? Voluptas distinctio sint deleniti quos quam inventore alias quas amet, ut est.</p>
+                <div id='divisionAyuda'>
+                    <a href="">Olvidé mi contraseña</a>  <a href="">Ayuda</a>
+                </div>
+            </section>
+        </div>
     );
 }
 export default Login;
